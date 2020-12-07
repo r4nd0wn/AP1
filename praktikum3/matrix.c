b5ec96a4e7ae2c4240fcb8185ef4fe6dee1cfcb1
r4nd0wn <jonas@jbach.xyz>
2020-12-07 10:56:20 +0100

    added specific manipulation of value

--- praktikum3/matrix.c
+++ praktikum3/matrix.c
@@ -12,7 +12,8 @@ void print_menu()
     printf("(6) Ende des Programms\n");
     printf("Eingabe: ");
 }
-void print_matrix(char matrixx[QUADRAT][QUADRAT]){
+void print_matrix(char matrixx[QUADRAT][QUADRAT])
+{
     printf("Die Matrix sieht so aus:\n\n");
     for (int i = 0; i < QUADRAT; i++)
     {
@@ -22,19 +23,42 @@ void print_matrix(char matrixx[QUADRAT][QUADRAT]){
         }
         printf("\n");
     }
-    printf("\n");
 }
 
-void diagonal_zero(char matrixx[][QUADRAT])
-{   
-    // einmal eine doppeltes setzen der Variablen, dafür nur 3 Zeilen Code.
-    for (int i = 0; i < QUADRAT; i++){
-        matrixx[i][i] = '0';
-        matrixx[i][QUADRAT -1 -i] = '0';
+void specific_zero(char matrixx[][QUADRAT])
+{
+    int x, y;
+
+    // Der durchschnittliche "Anwender" fängt bei 1 an zu Zählen, 
+    // daher werden Spalten und Zeilen ab 1 bis zur größe akzeptiert.
+
+    printf("Bitte geben sie die Spalte an (1 - %i): ", QUADRAT);
+    scanf("%i", &x);
+    printf("Bitte geben sie die Zeile an (1 - %i", QUADRAT);
+    scanf("%i", &y);
+    // sanity check:
+    if ((x < QUADRAT && y < QUADRAT) && (x > 0 && y > 0))
+    {
+        matrixx[x + 1][y + 1] = 0;
+        printf("Wert an Position %i, %i wurde auf 0 gesetzt")
+    }
+    else{
+        printf("Sie haben einen Wert außerhalb des Wertebereichs angegeben.\nDas Nullen des Feldes wurde nicht ausgeführt.")
     }
 }
 
-void fill_matrix(char matrixx[][QUADRAT], char fill_char){
+void diagonal_zero(char matrixx[][QUADRAT])
+{
+    // einmal eine doppeltes setzen der Variablen, dafür nur 3 Zeilen Code.
+    for (int i = 0; i < QUADRAT; i++)
+    {
+        matrixx[i][i] = '0';
+        matrixx[i][QUADRAT - 1 - i] = '0';
+    }
+}
+
+void fill_matrix(char matrixx[][QUADRAT], char fill_char)
+{
     for (int i = 0; i < QUADRAT; i++)
     {
         for (int j = 0; j < QUADRAT; j++)
@@ -47,10 +71,11 @@ void fill_matrix(char matrixx[][QUADRAT], char fill_char){
 int main()
 {
     char matrix[QUADRAT][QUADRAT];
-    
+
     fill_matrix(matrix, '+');
     print_matrix(matrix);
     print_menu();
+    printf("\n");
 
     int menu_blocker = 1, menu_selection;
     while (menu_blocker == 1)
@@ -67,7 +92,7 @@ int main()
             printf("\nEingabe: ");
             break;
         case 3:
-            printf("\nEingabe: ");
+            specific_zero();
             break;
         case 4:
             fill_matrix(matrix, '+');
@@ -79,7 +104,6 @@ int main()
             break;
         case 6:
             return 0;
-            break;
         default:
             printf("Dieser Menüpunkt ist leider invalid. Bitte probieren sie es noch mal.\n");
             print_menu();
