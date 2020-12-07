77b72935829f52c5be560f2b402d38eaf6e2dd3a
r4nd0wn <jonas@jbach.xyz>
2020-12-07 08:00:50 +0100

    added praktikum3

---
+++ praktikum3/matrix.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char matrix[5][5] = {"+"}
+    for (int i = 0; i<5; i++){
+        for (int j = 0; j<5; j++){
+            printf("%c ", matrix[i][j])
+        }
+    }
+}
---
+++ praktikum3/schaltjahr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int jahresinput;
+    printf("\nProgramm Schaltjahr | jbachma2\n\n");
+    printf("Bitte geben sie eine Jahreszahl ein: ");
+    scanf("%i", &jahresinput);
+    fflush(stdin);
+
+    if (jahresinput % 4 == 0 && !(jahresinput % 100 = 0)){
+        printf("\n%i ist ein Schaltjahr!\n\n", jahresinput);
+    }
+    else if (jahresinput % 100 == 0 && !(jahresinput % 400 = 0)){
+        printf("\n%i ist kein Schaltjahr!\n\n", jahresinput);
+    }
+    else if (jahresinput % 400 == 0){
+        printf("\n%i ist ein Schaltjahr!\n\n", jahresinput);
+    }
+    else {
+        printf("\nDa hat etwas nicht geklaptt :/\n");
+    }
+}
