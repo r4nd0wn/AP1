0774549c206a04d49c020f9954c9be1d94421403
r4nd0wn <jonas@jbach.xyz>
2020-12-07 08:05:33 +0100

    filling the array

--- praktikum3/matrix.c
+++ praktikum3/matrix.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-    char matrix[5][5] = {"+"}
+    char matrix[5][5];
     for (int i = 0; i<5; i++){
         for (int j = 0; j<5; j++){
-            printf("%c ", matrix[i][j])
+            matrix[i][j] = "+";
+        }
+    }
+    for (int i = 0; i<5; i++){
+        for (int j = 0; j<5; j++){
+            printf("%c ", matrix[i][j]);
         }
     }
 }
