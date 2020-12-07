2c834549bb54bfc80ced86aa2fa836cf262daffa
r4nd0wn <jonas@jbach.xyz>
2020-12-07 08:22:52 +0100

    added newline

--- praktikum3/matrix.c
+++ praktikum3/matrix.c
@@ -10,7 +10,7 @@ int main(){
     }
     for (int i = 0; i<5; i++){
         for (int j = 0; j<5; j++){
-            printf("%c ", matrix[i][j]);
+            printf("%c  ", matrix[i][j]);
         }
         printf("\n");
     }
