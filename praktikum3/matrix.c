db0ca221711898a1c42964baf0bbf802a08917d9
r4nd0wn <jonas@jbach.xyz>
2020-12-07 09:25:13 +0100

    removed size of array

--- praktikum3/matrix.c
+++ praktikum3/matrix.c
@@ -23,14 +23,13 @@ void print_matrix(char matrixx[QUADRAT][QUADRAT]){
     }
 }
 
-void diagonal_zero(char *matrixx[QUADRAT][QUADRAT])
+void diagonal_zero(char *matrixx)
 {   
     // einmal eine doppeltes setzen der Variablen, dafür nur 3 Zeilen Code.
     for (int i = 0; i < QUADRAT; i++){
         *matrixx[i][i] = 0;
         *matrixx[i][5-i] = 0;
     }
-    return matrixx;
 }
 
 int main()
