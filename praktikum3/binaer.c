b89629dd4e9122ab4028970e7c7460fc70e8899d
r4nd0wn <jonas@jbach.xyz>
2020-12-07 11:33:05 +0100

    more beautiful

--- praktikum3/binaer.c
+++ praktikum3/binaer.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define BITARRAY_LENGTH 8
 
-int main(){
+int main()
+{
     int eingabe;
     int dualzahl[BITARRAY_LENGTH];
+#define BITARRAY_LENGTH 8
 
     printf("Programm Dualzahl | jbachma2\n\n");
     printf("Geben Sie bitte eine ganze Zahl ein: ");
     scanf("%i", &eingabe);
-    for (int i = BITARRAY_LENGTH-1; i>=0; i--){
+    for (int i = BITARRAY_LENGTH - 1; i >= 0; i--)
+    {
         dualzahl[i] = eingabe % 2;
         eingabe = eingabe / 2;
     }
-    for (int i= 0 ; i<BITARRAY_LENGTH; i++){
+    for (int i = 0; i < BITARRAY_LENGTH; i++)
+    {
         printf("%i", dualzahl[i]);
     }
     printf("\n");
+    return 0;
 }
