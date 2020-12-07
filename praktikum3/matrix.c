9d4bce79d938c68089a3771dd318eae830d8c920
r4nd0wn <jonas@jbach.xyz>
2020-12-07 08:26:42 +0100

    added options

--- praktikum3/matrix.c
+++ praktikum3/matrix.c
@@ -8,10 +8,18 @@ int main(){
             matrix[i][j] = '+';
         }
     }
+    printf("Die MAtrix sieht so aus:\n")
     for (int i = 0; i<5; i++){
         for (int j = 0; j<5; j++){
-            printf("%c  ", matrix[i][j]);
+            printf("%c ", matrix[i][j]);
         }
         printf("\n");
     }
+    printf("Optionen:\n(1) Fülle alle Felder mit 0\n");
+    printf("(2) Fülle die beiden Diagonalen mit 0\n");
+    printf("(3) Fülle ein spezielles Feld mit 0 (Spalte, Zeile)\n");
+    printf("(4) Refresh: Fülle alle Felder wieder mit +\n");
+    printf("(5) Ausgabe der Matrix auf dem Bildschirm\n");
+    printf("(6) Ende des Programms\n")
+
 }
