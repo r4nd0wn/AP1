f407a513aa41a8401313bc0ce29b2bf087d40122
r4nd0wn <jonas@jbach.xyz>
2020-12-07 11:31:49 +0100

    I should rename the variables too xD

--- praktikum3/binaer.c
+++ praktikum3/binaer.c
@@ -5,13 +5,13 @@
 int main(){
     int eingabe;
     int dualzahl[BITARRAY_LENGTH];
-    
+
     printf("Programm Dualzahl | jbachma2\n\n");
     printf("Geben Sie bitte eine ganze Zahl ein: ");
     scanf("%i", &eingabe);
     for (int i = BITARRAY_LENGTH-1; i>=0; i--){
-        dualzahl[i] = ganzzahl % 2;
-        ganzzahl = ganzzahl / 2;
+        dualzahl[i] = eingabe % 2;
+        eingabe = eingabe / 2;
     }
     for (int i= 0 ; i<BITARRAY_LENGTH; i++){
         printf("%i", dualzahl[i]);
