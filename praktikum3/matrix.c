5d1ec6ac16548d9f6e06ffa6f1905413e4d7f6e9
r4nd0wn <jonas@jbach.xyz>
2020-12-07 08:22:24 +0100

    added newline

--- praktikum3/matrix.c
+++ praktikum3/matrix.c
@@ -5,7 +5,7 @@ int main(){
     char matrix[5][5];
     for (int i = 0; i<5; i++){
         for (int j = 0; j<5; j++){
-            matrix[i][j] = "+";
+            matrix[i][j] = '+';
         }
     }
     for (int i = 0; i<5; i++){
