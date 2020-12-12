56c4d3b4075a23ce75915a379668e0ca45b5df0d
r4nd0wn <jonas@jbach.xyz>
2020-12-12 22:34:08 +0100

    changed type to int

--- praktikum4/zaehlen.c
+++ praktikum4/zaehlen.c
@@ -23,14 +23,13 @@ int laengeZeichenkette(const char s[])
 int main()
 {
     char eingabe[200];
-    char entscheidung = 'j', suche;
-    int menu_blocker = 1;
+    char entscheidung, suche;
     printf("Bitte Namen eingeben: ");
     fflush(stdin);
     scanf(" %s", &eingabe);
     
     printf("%s hat %i Zeichen.\n", eingabe, laengeZeichenkette(eingabe));
-    while(entscheidung == 'j')
+    do
     {
         printf("Möchtest Du nach einem Buchstaben in %s suchen (j / n)? ", eingabe);
         scanf(" %c", &entscheidung);
@@ -40,7 +39,7 @@ int main()
             scanf(" %c", &suche);
             printf("Der buchstabe %c kommt %i-mal vor.\n", suche, sucheZeichen(eingabe, suche));
         }
-    }
+    }while(entscheidung == 'j')
     printf("Auf Wiedersehen!\n");
     return 0;
 }
