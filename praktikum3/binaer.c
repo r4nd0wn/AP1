66ca84f6ed1f443cc09e080d6bc30c5e7252b926
r4nd0wn <jonas@jbach.xyz>
2020-12-12 16:23:17 +0100

    added zaehlen.c

--- praktikum3/binaer.c
+++ praktikum3/binaer.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
-
 #define BITARRAY_LENGTH 8
-
-int main()
-{
+int main(){
     int eingabe;
     int dualzahl[BITARRAY_LENGTH];
-
-    printf("Programm Dualzahl | jbachma2\n\n");
-    printf("Geben Sie bitte eine ganze Zahl ein: ");
+    printf("Programm Dualzahl | jbachma2\n\nGeben Sie bitte eine ganze Zahl ein: ");
     scanf("%i", &eingabe);
     // sanity check, es dürfen nur Zahlen zwischen 0 und 255 eingegeben werden
-    if (eingabe >= 0 && eingabe <= 255){
-
+    if (eingabe >= 0 && eingabe <= 255){}
         // msb first, daher wird der loop bei dem höchsten Arraywert gestartet
-        for (int i = BITARRAY_LENGTH - 1; i >= 0; i--)
-        {
+        for (int i = BITARRAY_LENGTH - 1; i >= 0; i--){
             dualzahl[i] = eingabe % 2;
             eingabe = eingabe / 2;
         }
-        for (int i = 0; i < BITARRAY_LENGTH; i++)
-        {
+        for (int i = 0; i < BITARRAY_LENGTH; i++){
             printf("%i", dualzahl[i]);
         }
-        printf("\n");
     }
     else {
-        printf("Das hat nicht geklappt.\n");
-        printf("Bitte gib das nächste mal eine Ganzzahl x ein für die gilt: 0 >= x <= 255.\n");
+        printf("Das hat nicht geklappt.\nBitte gib das nächste mal eine Ganzzahl x ein für die gilt: 0 >= x <= 255.\n");
     }
     return 0;
 }
---
+++ praktikum4/zaehlen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define BITARRAY_LENGTH 8
+
+int sucheZeichen(const char s[], char c){
+    int counter = 0;
+    for(int i=0; i < sizeof(s); i++){
+        if(c == s[i]){
+            counter ++;
+        }
+    }
+    return counter;
+}
+
+int laengeZeichenkette(const char s[]){
+    return sizeof(s);
+}
+
+int main(){
+    char eingabe[];
+    char entscheidung, suche;
+    int menu_blocker = 1;
+    printf("Bitte Namen eingeben: ");
+    scanf("%s", &eingabe);
+    printf("%s hat %i Zeichen.", eingabe, laengeZeichenkette(eingabe));
+    do
+    {
+        printf("Möchtest Du nach einem Buchstaben in %s suchen (j / n)? ");
+        scanf("%c", &entscheidung);
+        if (entscheidung == 'j'){
+            printf("Bitte Buchstaben eingeben: ");
+            scanf("%c", &suche);
+            printf("Der buchstabe %c kommt %i-mal vor.\n", suche, sucheZeichen(eingabe, suche));
+        }
+    } while (entscheidung == 'j');
+    printf("Auf Wiedersehen!\n");
+    return 0;
+}
