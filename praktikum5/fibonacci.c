3173dc915e326cc4ca9da0e6029c5b93f84fbbd1
r4nd0wn <jonas@jbach.xyz>
2021-01-17 19:59:43 +0100

    fib

---
+++ praktikum5/fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+typedef struct Fibo_
+{
+    int n;
+    struct Fibo_ *next;
+} Fibo;
+
+int fibcalc(int n, int i, int sum)
+{
+    if (sum == 0){
+        i++;
+        fibcalc(0,i,sum + 1);
+    }
+    else{
+        i++;
+        fibcalc(0,i,sum+i);
+    }
+    return sum;
+    if (i <= n && i > 1){
+        printf("Die FibSumme bei n=%d ist: \t%d\n", i, sum);
+        i++;
+        fibcalc(n, i, sum + i);
+    }
+    else if(i == 1){
+        printf("Die FibSumme bei n=%d ist: \t%d\n", i, sum);
+        fibcalc(n, ++i, sum + 1);
+    }
+    else if(i == 0){
+        printf("Die FibSumme bei n=%d ist: \t%d\n", i, sum);
+        fibcalc(n, ++i, sum);
+    }
+    else{
+        return sum;
+    }
+}
+
+int main()
+{
+    while (1)
+    {
+        int f;
+        printf("Bitte geben sie eine Zahl für die fibonacci Berechnung ein: ");
+        scanf("%d", &f);
+        printf("ihre Fibonacci Zahl ist %d.\n", fibcalc(f, 0, 0));
+    }
+}
